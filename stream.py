c9f3ca692edfd33d25d976cc7946529db4c32110
alienx456 <estebanelias27dev@gmail.com>
2021-07-04 18:08:40 -0500

    solving dependency

--- stream.py
+++ stream.py
@@ -12,22 +12,20 @@ from kafka import KafkaConsumer, KafkaProducer
 logging.getLogger().setLevel(logging.INFO)
 inferencer_identifier = uuid.uuid4().__str__()
 
+try:
+    consumer = KafkaConsumer(os.environ['DATA_UPLOAD_EVENT'],
+                             group_id=os.environ['GROUP_ID'],
+                             bootstrap_servers=[os.environ['KAFKA_BOOTSTRAP_SERVER_ONE']],
+                             auto_offset_reset='earliest',
+                             enable_auto_commit='true',
+                             client_id=inferencer_identifier)
+    producer = KafkaProducer(bootstrap_servers=[os.environ['KAFKA_BOOTSTRAP_SERVER_ONE']],
+                             value_serializer=lambda x: dumps(x).encode(os.environ['ENCODE_FORMAT']))
 
+    logging.info('Started download of model configuration and weights')
 
-consumer = KafkaConsumer(os.environ['DATA_UPLOAD_EVENT'],
-                         group_id=os.environ['GROUP_ID'],
-                         bootstrap_servers=[os.environ['KAFKA_BOOTSTRAP_SERVER_ONE']],
-                         auto_offset_reset='earliest',
-                         enable_auto_commit='true',
-                         client_id=inferencer_identifier)
-producer = KafkaProducer(bootstrap_servers=[os.environ['KAFKA_BOOTSTRAP_SERVER_ONE']],
-                         value_serializer=lambda x: dumps(x).encode(os.environ['ENCODE_FORMAT']))
+    result = YOLOResources.download_yolo_resources()
 
-logging.info('Started download of model configuration and weights')
-
-result = YOLOResources.download_yolo_resources()
-
-if (result):
 
     aws_resorce = AwsS3Resource()
 
@@ -41,17 +39,17 @@ if (result):
         fileName = message.value.decode(os.environ['ENCODE_FORMAT'])
         logging.info("New IMG arrived ID %s to consumer %s", fileName, inferencer_identifier)
         try:
-            img = aws_resorce.load_image('fileName')
+            img = aws_resorce.load_image(fileName)
             logging.info(len(img))
             nparray = np.asarray(bytearray(img), dtype="uint8")
             decoded_img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
             img_darknet = Image(decoded_img)
             results = net.detect(img_darknet)
-            dataToSend = {'device_info': {'data_uuid': fileName, 'index_name': os.getenv('ELASTIC_INDEX_NAME')}, 'image_classification': results}
-            producer.send(topic=os.getenv['PROCESS_RESULT_EVENT'], value=dataToSend)
+            dataToSend = {'device_info': {'data_uuid': fileName, 'index_name': os.getenv('ELASTIC_INDEX_NAME')},
+                          'image_classification': results}
+            producer.send(topic=os.getenv('PROCESS_RESULT_EVENT'), value=dataToSend)
         except Exception as e:
             logging.error(e)
 
-else:
-    logging.info('There was a problem downloading model configuration and weights')
-
+except Exception as e:
+    logging.error('There was an error while Connecting: %s', str(e))
