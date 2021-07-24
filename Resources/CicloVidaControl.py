14776d7a1fe1b2bcb60dea388360c6a8a65d280e
alienx456 <estebanelias27dev@gmail.com>
2021-07-23 23:20:17 -0500

    refactor -> move data process

--- Resources/CicloVidaControl.py
+++ Resources/CicloVidaControl.py
@@ -1,11 +1,32 @@
+import cv2
+import logging
+import numpy as np
 from Resources.AwsS3Resource import AwsS3Resource
+from pydarknet import Detector, Image
 
+logging.getLogger().setLevel(logging.INFO)
 class CicloVidaControl:
 
     def __init__(self):
         self.__aws_s3_resource = AwsS3Resource()
+        self.__net = Detector(bytes("./cfg/yolov3.cfg", encoding="utf-8"), bytes("./weights/yolov3.weights", encoding="utf-8"),
+                       0,
+                       bytes("./data/coco.data", encoding="utf-8"))
 
     def process_audio(self, nombre_archivo):
-        self.__aws_s3_resource.download_object(nombre_archivo)
+        img = self.__aws_s3_resource.load_image(nombre_archivo)
+        nparray = np.asarray(bytearray(img), dtype="uint8")
+        decoded_img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
+        img_darknet = Image(decoded_img)
+        detections = self.__net.detect(img_darknet)
+        return self.remove_box_on_detections(detections)
+
+    @staticmethod
+    def remove_box_on_detections(detections) -> dict:
+        detections_filtered = []
+        for detection in detections:
+            detections_filtered.append({"class": detection[0], "score": detection[1]})
+        return detections_filtered
+
 
 
--- stream.py
+++ stream.py
@@ -1,12 +1,9 @@
-import cv2
 import logging
 import os
 import uuid
-import numpy as np
 from json import dumps
-from pydarknet import Detector, Image
-from Resources.AwsS3Resource import AwsS3Resource
 from kafka import KafkaConsumer, KafkaProducer
+from Resources.CicloVidaControl import  CicloVidaControl
 from datetime import datetime
 
 logging.getLogger().setLevel(logging.INFO)
@@ -14,11 +11,10 @@ identifier = uuid.uuid4().__str__()
 
 try:
 
-    aws_resorce = AwsS3Resource()
 
     logging.info('Starting detector...')
-    net = Detector(bytes("./cfg/yolov3.cfg", encoding="utf-8"), bytes("./weights/yolov3.weights", encoding="utf-8"), 0,
-                   bytes("./data/coco.data", encoding="utf-8"))
+
+    data_cycle = CicloVidaControl()
 
     consumer = KafkaConsumer(os.environ['DATA_UPLOAD_EVENT'],
                              group_id=os.environ['GROUP_ID'],
@@ -35,12 +31,7 @@ try:
         logging.info("New IMG arrived ID %s to consumer %s", fileName, identifier)
         try:
             startTime = datetime.now()
-            img = aws_resorce.load_image(fileName)
-            logging.info(len(img))
-            nparray = np.asarray(bytearray(img), dtype="uint8")
-            decoded_img = cv2.imdecode(nparray, cv2.IMREAD_COLOR)
-            img_darknet = Image(decoded_img)
-            results = net.detect(img_darknet)
+            results = data_cycle.process_audio(fileName)
             dataToSend = {'device_info': {'data_uuid': fileName, 'index_name': os.getenv('ELASTIC_INDEX_NAME'),
                                           'location-lat': 0.00000, 'location-lon': 0.00000},
                           'image_classification': results}
